33b723c0e76ece4028e6f73f7be0da517032efcf
Ckmena <Ckmena@users.noreply.github.com>
2021-01-27 19:27:47 +0000

    deploy: 41b0a2e15179bf876c37c58e2d55a390ed4992f0

---
+++ precache-manifest.0a43485b0ad53c4a25094864cdc0e880.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5becbd1ed2740f0ca532eda7c36540",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6940820e570a96c6ca94",
+    "url": "/static/css/main.e98ed1b6.chunk.css"
+  },
+  {
+    "revision": "dd1e34ae99691344d883",
+    "url": "/static/js/2.87595c4e.chunk.js"
+  },
+  {
+    "revision": "107a53f487bc8e3a094f03dee1c9f700",
+    "url": "/static/js/2.87595c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6940820e570a96c6ca94",
+    "url": "/static/js/main.3346e720.chunk.js"
+  },
+  {
+    "revision": "e2bc7bd2f95d88b3f2ac",
+    "url": "/static/js/runtime-main.9dcada38.js"
+  }
+]);
